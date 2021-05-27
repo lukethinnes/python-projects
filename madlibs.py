ede29ebd82d1dc661638d8a0c571b2a5000bc092
Luke Thinnes <lukethinnes@yahoo.com>
2021-05-27 12:21:33 -0600

    Initial commit.

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# Use string concatenation to make a madlib.
+
+adj1 = input('Adjective: ')
+adj2 = input('Another adjective: ')
+adj3 = input('One last adjective: ')
+noun1 = input('Noun: ')
+noun2 = input('Another noun: ')
+noun3 = input('One final noun: ')
+
+print(f'The {adj1} {noun1} looks {adj2} on this fine {noun2}. Who would have thought {noun3} could be so {adj3}?!')
